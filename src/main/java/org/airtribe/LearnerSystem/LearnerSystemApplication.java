a5c09dec4e9bb7a799999b7f7c4c8bd517849b78
Shivam naithani <shivamnaithani0123@gmail.com>
2026-06-09 17:46:44 +0530

    Initial commit

---
+++ src/main/java/org/airtribe/LearnerSystem/LearnerSystemApplication.java
@@ -0,0 +1,13 @@
+package org.airtribe.LearnerSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnerSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnerSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/airtribe/LearnerSystem/LearnerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.airtribe.LearnerSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnerSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
