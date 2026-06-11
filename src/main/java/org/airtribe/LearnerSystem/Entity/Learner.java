2d853c7f541cce2fa0bf8d506a018e60101b6286
shivamnaithani-act21 <shivam.naithani@act21.io>
2026-06-11 16:32:01 +0530

    Add Spring Data JPA dependency, create Learner entity and controller, connect to H2 database, and map Learner entity to table

---
+++ src/main/java/org/airtribe/LearnerSystem/Entity/Learner.java
@@ -0,0 +1,52 @@
+package org.airtribe.LearnerSystem.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Learner {
+    @Id
+    private Long learnerID;
+    private String name;
+    private String username;
+    private String password;
+    public Learner(Long learnerID, String name, String username, String password) {
+        this.learnerID = learnerID;
+        this.name = name;
+        this.username = username;
+        this.password = password;
+
+    }
+
+    public Long getLearnerID() {
+        return learnerID;
+    }
+
+    public void setLearnerID(Long learnerID) {
+        this.learnerID = learnerID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
